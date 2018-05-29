b76753b01cda41815221a87cc0d1dd0767a3dc97
cbassp <perez.arcila.sebastian@gmail.com>
2018-05-29 14:00:57 +0200

    Adivinar un numero 5 veces

---
+++ adivina_numero.py
@@ -0,0 +1,26 @@
+numero_para_adivinar = 4
+numero_del_usuario = int(input('Adivina el numero: '))
+
+if numero_para_adivinar == numero_del_usuario:
+    print('has acertado')
+else:
+    numero_del_usuario = int(input('Adivina el numero: '))
+    if numero_para_adivinar == numero_del_usuario:
+        print('has acertado')
+    else:
+        numero_del_usuario = int(input('Adivina el numero: '))
+        if numero_para_adivinar == numero_del_usuario:
+            print('has acertado')
+        else:
+            numero_del_usuario = int(input('Adivina el numero: '))
+            if numero_para_adivinar == numero_del_usuario:
+                print('has acertado')
+            else:
+                numero_del_usuario = int(input('Adivina el numero: '))
+                if numero_para_adivinar == numero_del_usuario:
+                    print('has acertado')
+                else:
+                    print('has superado el numero de intentos')
+
+
+
