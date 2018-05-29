08c8dbd58c42d4c7a826198efbb563c4ec17a7df
cbassp <perez.arcila.sebastian@gmail.com>
2018-05-29 23:10:39 +0200

    ejercicio while

---
+++ example_while.py
@@ -0,0 +1,15 @@
+#numero_inicial = 10
+
+#while numero_inicial > 0:
+#    numero_inicial -= 1
+#    print(numero_inicial)
+
+#print('He terminado')
+
+#Ejercicio resuelto para mostrar el 10 y el 0:
+
+numero_inicial = 10
+
+while numero_inicial >= 0:
+    print(numero_inicial)
+    numero_inicial -= 1
