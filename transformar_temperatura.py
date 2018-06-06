421db8cfafdf212112f13f044a540b721b1a5a85
cbassp <perez.arcila.sebastian@gmail.com>
2018-06-06 20:20:11 +0200

    transformar temperatura

---
+++ transformar_temperatura.py
@@ -0,0 +1,4 @@
+farenheit = int(input('Introduce la temperatura en grados Farenheit para pasarla a grados Celcius: '))
+celcius = (farenheit - 32) / 1.8
+
+print('{} grados Farenheit son {} grados Celcius'.format(farenheit, celcius))
